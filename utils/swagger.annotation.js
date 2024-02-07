9a6548871106551c25b1168d707ba3bbe9b45acc
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-02-07 18:21:37 +0100

    swagger docs endpoint description

--- utils/swagger.annotation.js
+++ utils/swagger.annotation.js
@@ -3,7 +3,15 @@ const swaggerDefinitions = {
       post: {
         tags: ['Auth'],
         summary: 'Create a new user',
-        description: 'Create a new user.',
+        description: `|
+          This endpoint allows you to create a new user account.
+          To create a user, you must provide the following information: |
+          
+          - email: The email address of the user. This field is required.
+          - password: The password for the user account. This field is required.
+          - name: The name for the user account. This field is required.
+          
+          Upon successful creation, the endpoint returns a response with status code 201 (Created).`,
         requestBody: {
           description: 'User object',
           required: true,
@@ -34,7 +42,18 @@ const swaggerDefinitions = {
         post: {
           tags: ['Auth'], 
           summary: 'Login user',
-          description: 'Login user.',
+          description: `| 
+            This endpoint allows users to log in to their accounts.
+            To authenticate, users must provide their email and password. |
+
+            - email: The email address of the user. This field is required.
+            - password: The password for the user account. This field is required.
+
+            Upon successful authentication, the endpoint returns a response with status code 200 (OK) along with an authentication token.
+
+            Note: The authentication token should be included in the headers of subsequent requests to authenticate the user.
+
+              `,
           requestBody: {
             description: 'User object',
             required: true,
@@ -63,7 +82,15 @@ const swaggerDefinitions = {
         post: {
           tags: ['Auth'], 
           summary: 'Request OTP',
-          description: 'request otp',
+          description: `| 
+            This endpoint allows users to request a one-time password (OTP) for authentication purposes.
+            Users must provide their email address to receive the OTP.
+            Upon successful request, the OTP is sent to the provided email address. |
+            
+            - email: The email address of the user. This field is required.
+
+            Note: The OTP expires after a certain period and can only be used once for authentication.
+          `,
           requestBody: {
             description: 'OTP object',
             required: true,
@@ -91,7 +118,14 @@ const swaggerDefinitions = {
         post: {
           tags: ['Auth'], 
           summary: 'Confirm OTP',
-          description: 'confirm otp',
+          description: `|
+            This endpoint allows users to confirm the one-time password (OTP) they received for authentication purposes.
+            Users must provide the OTP they received via email. |
+    
+            Upon successful confirmation, the user's authentication is validated and they can proceed with their desired action.
+    
+            Note: The OTP expires after a certain period and can only be used once for authentication.
+          `,
           requestBody: {
             description: 'OTP object',
             required: true,
@@ -120,7 +154,12 @@ const swaggerDefinitions = {
         patch: {
           tags: ['Auth'], 
           summary: 'Request password reset',
-          description: 'request password reset',
+          description: `|
+            This endpoint allows users to request a password reset by providing their email address.
+            Upon successful request, a password reset OTP will be sent to the provided email address. |
+
+            Note: The password reset OTP is valid for a limited time period and can only be used once.
+          `,
           requestBody: {
             description: 'Password object',
             required: true,
@@ -148,7 +187,12 @@ const swaggerDefinitions = {
         post: {
           tags: ['Auth'], 
           summary: 'Confirm request password',
-          description: 'confirm request password',
+          description: `| 
+            This endpoint allows users to confirm a password reset request by providing a new password and a token received via email.
+            Upon successful confirmation, the user's password will be updated to the new password. |
+    
+            Note: The password reset token expires after a certain period and can only be used once.
+          `,
           requestBody: {
             description: 'Password object',
             required: true,
@@ -178,7 +222,16 @@ const swaggerDefinitions = {
         post: {
           tags: ['Auth'], 
           summary: 'Google Auth',
-          description: 'This endpoint create a new user, if user exist already, it login the user',
+          description: `|
+            This endpoint allows you to create a new user account with google authentication.
+            To create a user, you must provide the following information: 
+            if the email exist then the user is login. |
+            
+            - email: The email address of the user. This field is required.
+            - name: The name for the user account. This field is required.
+            - accessToken: The access token from google. This field is required.
+            
+            Upon successful creation, the endpoint returns a response with status code 201 (Created).`,
           requestBody: {
             description: 'Password object',
             required: true,
@@ -199,7 +252,7 @@ const swaggerDefinitions = {
             }
           },
           responses: {
-            '200': {
+            '201': {
               description: 'User created successfully'
             }
           }
