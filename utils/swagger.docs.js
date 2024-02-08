41faa73cfe28522bfea27ea61515a9829cde3bff
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2024-02-08 11:32:18 +0100

    swagger docs dev server

--- utils/swagger.docs.js
+++ utils/swagger.docs.js
@@ -2,30 +2,39 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const swaggerAnnotation = require("../utils/swagger.annotation")
 
-module.exports = (app) => {    
-    const options = {
-        definition: {
-            openapi: '3.0.0',
-            info: {
-            title: 'PlaySphere',
-            version: '1.0.0',
-            description: 'PlaySphere Documentation',
-            },
-            servers: [
-            {
-                url: true ? "http://127.0.0.1:3000/api/v1" : null,
-                description: 'Development server',
-            },
-            ],
-            paths: swaggerAnnotation,
-            tags: [
-                { name: 'Auth', description: 'Endpoints related to user authentication' }
-            ]
-        },
-        apis: ['index.js'],
-    };
-    
-    const setup = swaggerJsdoc(options);
+module.exports = (app) => { 
+    app.use((req, res, next) => {
+        var localhost = true
+        if(req.hostname != "127.0.0.1" && req.hostname != "localhost"){
+            localhost = false
+        }
 
-    app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(setup));
+        next()
+        
+        const options = {
+            definition: {
+                openapi: '3.0.0',
+                info: {
+                title: 'PlaySphere',
+                version: '1.0.0',
+                description: 'PlaySphere Documentation',
+                },
+                servers: [
+                {
+                    url: localhost ? "http://127.0.0.1:3000/api/v1" : "https://playshere-api-v1.onrender.com/api/v1",
+                    description: 'Development server',
+                },
+                ],
+                paths: swaggerAnnotation,
+                tags: [
+                    { name: 'Auth', description: 'Endpoints related to user authentication' }
+                ]
+            },
+            apis: ['index.js'],
+        };
+        
+        const setup = swaggerJsdoc(options);
+
+        app.use('/api/v1/api-docs', swaggerUi.serve, swaggerUi.setup(setup));
+    })
 };
